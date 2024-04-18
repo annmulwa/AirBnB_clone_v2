6738355ee50b3332571230d3e125f6727019dd99
annmulwa <annmulwa2000@gmail.com>
2024-04-18 09:53:43 -0700

    web flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+script that starts a Flask web application
+'''
+from flask import Flask, render_template
+from models import *
+from models import storage
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    '''
+    remove the current SQLAlchemy Session
+    '''
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    '''
+    displays an html page with state list sorted by name
+    '''
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
